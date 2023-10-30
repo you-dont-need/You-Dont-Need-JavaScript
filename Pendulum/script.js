cccad3151cd12b6656ec291a1f44f39677ad2f50
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2023-10-31 01:26:02 +0530

    Delete Pendulum/script.js

--- Pendulum/script.js
+++
@@ -1,9 +0,0 @@
-const speedSlider = document.getElementById('speed-slider');
-const speedValue = document.getElementById('speed-value');
-const pendulum = document.querySelector('.pendulum');
-
-speedSlider.addEventListener('input', () => {
-    const speed = speedSlider.value;
-    speedValue.textContent = speed;
-    pendulum.style.animationDuration = `${1 / speed}s`;
-});
